845f5adfcafa3ec9317c66ee41b8ddaaf7c52ca2
code-sum <yisugyeong@gmail.com>
2022-10-17 22:10:12 +0900

    Image upload - add field

--- articles/models.py
+++ articles/models.py
@@ -12,3 +12,4 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', blank=True)
