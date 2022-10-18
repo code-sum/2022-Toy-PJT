242bf0667118d3adad0132255cc235e607d3430d
code-sum <yisugyeong@gmail.com>
2022-10-18 22:54:43 +0900

    Apply datetime module

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from imagekit.models import ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill
 from django.db import models
+from datetime import datetime, timedelta, timezone
 
 '''
 게시판 기능
@@ -21,8 +22,53 @@ class Article(models.Model):
     thumbnail = ImageSpecField(source='image', 
                                 processors=[ResizeToFill(800,600)], 
                                 format='JPEG')
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return False
+    def updated_string(self):
+        time = datetime.now(tz=timezone.utc) - self.updated_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.updated_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return False
 
 class Comment(models.Model):
     content = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return False
